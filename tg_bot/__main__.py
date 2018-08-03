da5f7a6c6c8f91ef6fefa597817fe352d6803bc9
raza231198 <raza231198@gmail.com>
2018-08-03 01:23:32 +0000

    jdjd

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -32,7 +32,7 @@ the things I can help you with.
  - /start: start the bot
  - /help: PM's you this message.
  - /help <module name>: PM's you info about that module.
- - /credits: information about how to donate!
+ - /credits: See who contribuited the bot.
  - /settings:
    - in PM: will send you your settings for all supported modules.
    - in a group: will redirect you to pm, with all that chat's settings.
